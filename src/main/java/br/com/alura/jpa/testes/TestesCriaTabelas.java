04dfe7928e26f658e6b696a778769177a709a513
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2020-09-26 16:01:26 -0300

    primeiro commit

---
+++ src/main/java/br/com/alura/jpa/testes/TestesCriaTabelas.java
@@ -0,0 +1,17 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class TestesCriaTabelas {
+
+	private static EntityManager createEntityManager;
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager createEntityManager = emf.createEntityManager();
+		emf.close();
+	}
+
+}
