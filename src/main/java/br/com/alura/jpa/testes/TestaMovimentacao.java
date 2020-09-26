69c88050f7fb57aeccde1ca3f030116bf36d0c40
Eduardo Fernando Dias <dudu.dias74@gmail.com>
2020-09-26 17:21:53 -0300

    segundo commit

---
+++ src/main/java/br/com/alura/jpa/testes/TestaMovimentacao.java
@@ -0,0 +1,40 @@
+package br.com.alura.jpa.testes;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+import br.com.alura.jpa.modelo.Movimentacao;
+import br.com.alura.jpa.modelo.TipoMovimentacao;
+
+public class TestaMovimentacao {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.setAgencia(112233);
+		conta.setNumero(334455);
+		conta.setSaldo(300.00);
+		conta.setTitular("Leonardo");
+		
+		Movimentacao movimentacao = new Movimentacao();
+		movimentacao.setData(LocalDateTime.now());
+		movimentacao.setDescricao("Churrascaria");
+		movimentacao.setValor(new BigDecimal(200.00));
+		movimentacao.setTipoMovimentacao(TipoMovimentacao.SAIDA);
+		movimentacao.setConta(conta);
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(conta);
+		em.persist(movimentacao);
+		em.getTransaction().commit();
+		
+		em.close();
+	}
+
+}
